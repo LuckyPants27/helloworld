a9aa0ebd7f3394bfccff09c58e9df9a4cd41b0a4
LuckyPants27 <hitori-a@yandex.ru>
2024-01-12 22:34:41 +0500

    Message changed to "Привет, Мир!"

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+//        SpringApplication.run(HelloworldApplication.class, args);
+        System.out.println("Привет, Мир!");
     }
 
 }
