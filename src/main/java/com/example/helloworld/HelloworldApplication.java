ac3a790eec23b7cb51826a3780005f19fad3bbe8
LuckyPants27 <hitori-a@yandex.ru>
2024-01-16 06:33:43 +0500

    Deleted unused imports

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -3,7 +3,6 @@ package com.example.helloworld;
 public class HelloworldApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(HelloworldApplication.class, args);
         System.out.println("Привет, Мир!!!");
         System.out.println("Hello, World!!!");
         System.out.println("Hello, World!!!");
